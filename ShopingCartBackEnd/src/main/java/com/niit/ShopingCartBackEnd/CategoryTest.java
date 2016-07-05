f52fe5dfecdf82d9e91bd7eed94c1d8692a7eaf3
divya290994 <divyan746@gmail.com>
2016-07-05 18:04:49 +0530

    after integration backend

---
+++ ShopingCartBackEnd/src/main/java/com/niit/ShopingCartBackEnd/CategoryTest.java
@@ -0,0 +1,46 @@
+package com.niit.ShopingCartBackEnd;
+
+import java.util.List;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.niit.shopingcart.dao.CategoryDAO;
+import com.niit.shopingcart.modal.Category;
+
+public class CategoryTest {
+	
+	static AnnotationConfigApplicationContext context;
+	
+	public static void main(String[] args) {
+		
+		
+	    context = new AnnotationConfigApplicationContext();
+		context.scan("com.niit.shopingcart.config");
+		context.refresh();
+		
+	Category c =(Category)	  context.getBean("category");
+	
+	CategoryDAO categoryDAO = (CategoryDAO)  context.getBean("categoryDAO");
+	
+/*	
+	c.setId("TAB_003");
+	c.setName("Tablet");
+	c.setDescription("Tablet product");
+	
+	
+	categoryDAO.saveOrUpdate(c);
+	*/
+	
+	
+	
+	List<Category>  list =    categoryDAO.list();
+	
+	for(Category cat : list)
+	{
+		System.out.println(cat.getId()  + ":" +  cat.getName()  + ":"+  cat.getDescription());
+	}
+		
+		
+	}
+
+}
---
+++ ShopingCartBackEnd/src/main/java/com/niit/shopingcart/dao/UserDAO.java
@@ -0,0 +1,24 @@
+package com.niit.shopingcart.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+
+public class UserDAO{
+	
+		public boolean isvalidUser(String name,String password)
+		{
+			if(name.equals("niit")&&password.equals("niit@123"))
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+			}
+			
+		}
+
+
+
